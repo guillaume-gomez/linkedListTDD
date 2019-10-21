6459a318ce0e56f8fbbd7f88bce137d32c8e9892
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-10-21 13:49:54 +0200

    remove code and comment tests

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -11,23 +11,17 @@ export const emptyLinkedList: LinkedList = { head: null };
 
 // create a new node
 export function createNode(value: string) : Node {
-  return { value, next: null };
+  return;
 }
 
 // create a new linked list
 export function createLinkedList(firstValue: string) : LinkedList {
-  return { head: createNode(firstValue) };
+  return;
 }
 
 // insert a new node after a node in a linked list
 export function insertAfter(node: Node, value: string) : Node {
-  let newNode = createNode(value);
-  let oldNode = node.next;
-
-  newNode.next = oldNode;
-  node.next = newNode;
-
-  return newNode;
+  return;
 }
 
 // insert after tail
@@ -37,33 +31,15 @@ export function append(linkedList: LinkedList, value: string) : Node {
 }
 
 export function length(linkedList: LinkedList) : number {
-  if(linkedList.head === null) {
-    return 0;
-  };
-  let len = 0;
-  let iterator = linkedList.head;
-  while(iterator) {
-    len = len + 1;
-    iterator = iterator.next;
-  }
-  return len;
+  return -1;
 }
 
 export function tail(linkedList: LinkedList) : Node {
-  let tail = linkedList.head;
-
-  while(tail.next !== null) {
-    tail = tail.next;
-  }
-  return tail;
+  return;
 }
 
 export function findNode(linkedList: LinkedList, value: string) : Node {
-  let iterator = linkedList.head;
-  while(iterator && iterator.value !== value) {
-    iterator = iterator.next;
-  }
-  return iterator;
+  return;
 }
 
 export function removeAfter(linkedList: LinkedList, node: Node): Node {
@@ -88,19 +64,11 @@ export function removeHead(linkedList: LinkedList) : Node {
 }
 
 export function forEach(linkedList: LinkedList, callback: (value: string, index: number) => void) : void {
-  let iterator = linkedList.head;
-  let index = 0;
-  while(iterator) {
-    callback(iterator.value, index);
-    index = index + 1;
-    iterator = iterator.next;
-  }
+
 }
 
 export function print(linkedList: LinkedList) : string {
-  let results : string[] = [];
-  forEach(linkedList, (value) => { results.push(value) } );
-  return results.join(", ");
+  return ""
 }
 
 
--- src/components/__tests__/LinkedList.spec.ts
+++ src/components/__tests__/LinkedList.spec.ts
@@ -21,129 +21,129 @@ it("create an empty linked list", () => {
   expect(emptyLinkedList.head).toBe(null);
 });
 
-it("create a node", () => {
-  expect(createNode("a new node")).toEqual({ value: "a new node", next: null});
-});
+// it("create a node", () => {
+//   expect(createNode("a new node")).toEqual({ value: "a new node", next: null});
+// });
 
-it("create an linked list with only one item", () => {
-  const linkedList = createLinkedList("first node");
-  expect(linkedList.head.value).toBe("first node");
-  expect(linkedList.head.next).toBe(null);
-});
+// it("create an linked list with only one item", () => {
+//   const linkedList = createLinkedList("first node");
+//   expect(linkedList.head.value).toBe("first node");
+//   expect(linkedList.head.next).toBe(null);
+// });
 
-it("should insert a node after a specific node in linkedlist", () => {
-  const linkedList = createLinkedList("first node");
-  const secondNode = insertAfter(linkedList.head, "second node");
-  expect(secondNode.value).toBe("second node")
-  expect(linkedList.head.next).toBe(secondNode)
-});
+// it("should insert a node after a specific node in linkedlist", () => {
+//   const linkedList = createLinkedList("first node");
+//   const secondNode = insertAfter(linkedList.head, "second node");
+//   expect(secondNode.value).toBe("second node")
+//   expect(linkedList.head.next).toBe(secondNode)
+// });
 
-it("get the linked list length", () => {
-  expect(length(emptyLinkedList)).toBe(0)
-  const linkedList = createLinkedList("first node");
-  expect(length(linkedList)).toBe(1);
+// it("get the linked list length", () => {
+//   expect(length(emptyLinkedList)).toBe(0)
+//   const linkedList = createLinkedList("first node");
+//   expect(length(linkedList)).toBe(1);
 
-  const secondNode = insertAfter(linkedList.head, "second node");
-  const thirdNode = insertAfter(secondNode, "third node");
-  expect(length(linkedList)).toBe(3);
-});
+//   const secondNode = insertAfter(linkedList.head, "second node");
+//   const thirdNode = insertAfter(secondNode, "third node");
+//   expect(length(linkedList)).toBe(3);
+// });
 
-it("get the tail", () => {
-  const linkedList = createLinkedList("first node");
-  const secondNode = insertAfter(linkedList.head, "second node");
-  const thirdNode = insertAfter(secondNode, "third node");
+// it("get the tail", () => {
+//   const linkedList = createLinkedList("first node");
+//   const secondNode = insertAfter(linkedList.head, "second node");
+//   const thirdNode = insertAfter(secondNode, "third node");
   
-  expect(tail(linkedList)).toEqual(thirdNode);
+//   expect(tail(linkedList)).toEqual(thirdNode);
 
-});
+// });
 
-it("find value in one item linked list", () => {
-  const linkedList = createLinkedList("first node");
-  expect(length(linkedList)).toBe(1);
+// it("find value in one item linked list", () => {
+//   const linkedList = createLinkedList("first node");
+//   expect(length(linkedList)).toBe(1);
 
-  const node = findNode(linkedList, "first node");
-  expect(node.value).toBe("first node");
+//   const node = findNode(linkedList, "first node");
+//   expect(node.value).toBe("first node");
 
-  const nodeNotFound = findNode(linkedList, "no existing node");
-  expect(nodeNotFound).toBe(null);
-});
+//   const nodeNotFound = findNode(linkedList, "no existing node");
+//   expect(nodeNotFound).toBe(null);
+// });
 
 
-it("use forEach in a linked list", () => {
-  const linkedList = createLinkedList("first node");
-  const secondNode = append(linkedList, "second node");
-  const thirdNode = append(linkedList, "third node");
+// it("use forEach in a linked list", () => {
+//   const linkedList = createLinkedList("first node");
+//   const secondNode = append(linkedList, "second node");
+//   const thirdNode = append(linkedList, "third node");
   
-  let expectedResult = ["node 0", "node 1", "node 2"];
-  let result : string[] = [];
-  forEach(linkedList, (value, index) => {
-    const computedValue = `${value.slice(value.length - 4)} ${index}`;
-    result.push(computedValue);
-  });
-  expect(expectedResult).toEqual(result);
+//   let expectedResult = ["node 0", "node 1", "node 2"];
+//   let result : string[] = [];
+//   forEach(linkedList, (value, index) => {
+//     const computedValue = `${value.slice(value.length - 4)} ${index}`;
+//     result.push(computedValue);
+//   });
+//   expect(expectedResult).toEqual(result);
 
-});
+// });
 
-it("use print fonction to display the linked list values", () => {
-  const linkedList = createLinkedList("1");
-  const secondNode = append(linkedList, "23");
-  const thirdNode = append(linkedList, "456");
+// it("use print fonction to display the linked list values", () => {
+//   const linkedList = createLinkedList("1");
+//   const secondNode = append(linkedList, "23");
+//   const thirdNode = append(linkedList, "456");
 
-  const expectedResult = "1, 23, 456";
-  expect(print(linkedList)).toEqual(expectedResult);
-});
+//   const expectedResult = "1, 23, 456";
+//   expect(print(linkedList)).toEqual(expectedResult);
+// });
 
 
-it("manipulate a linked list", () => {
-  const linkedList = createLinkedList("first node");
-  const secondNode = append(linkedList, "second node");
-  expect(length(linkedList)).toBe(2);
+// it("manipulate a linked list", () => {
+//   const linkedList = createLinkedList("first node");
+//   const secondNode = append(linkedList, "second node");
+//   expect(length(linkedList)).toBe(2);
 
-  const thirdNode = append(linkedList, "third node");
-  expect(length(linkedList)).toBe(3);
+//   const thirdNode = append(linkedList, "third node");
+//   expect(length(linkedList)).toBe(3);
 
-  const deletedNode = removeHead(linkedList);
-  expect(deletedNode.value).toBe("first node");
-  expect(linkedList.head.value).toBe("second node");
+//   const deletedNode = removeHead(linkedList);
+//   expect(deletedNode.value).toBe("first node");
+//   expect(linkedList.head.value).toBe("second node");
 
-  expect(length(linkedList)).toBe(2);
+//   expect(length(linkedList)).toBe(2);
 
-  const fourthNode = append(linkedList, "fourth node");
-  expect(length(linkedList)).toBe(3);
-  const deletedNodeTail = removeAfter(linkedList, thirdNode);
-  expect(deletedNodeTail).toBe(fourthNode);
-});
+//   const fourthNode = append(linkedList, "fourth node");
+//   expect(length(linkedList)).toBe(3);
+//   const deletedNodeTail = removeAfter(linkedList, thirdNode);
+//   expect(deletedNodeTail).toBe(fourthNode);
+// });
 
-it("find values in a manipulated linked list", () => {
-  const linkedList = createLinkedList("first node");
-  const secondNode = append(linkedList, "second node");
-  const thirdNode = append(linkedList, "third node");
+// it("find values in a manipulated linked list", () => {
+//   const linkedList = createLinkedList("first node");
+//   const secondNode = append(linkedList, "second node");
+//   const thirdNode = append(linkedList, "third node");
   
-  const node = findNode(linkedList, "second node");
-  expect(node.value).toBe("second node");
+//   const node = findNode(linkedList, "second node");
+//   expect(node.value).toBe("second node");
 
-  const deletedNode = removeHead(linkedList);
-  expect(deletedNode.value).toBe("first node");
-  expect(linkedList.head.value).toBe("second node");
+//   const deletedNode = removeHead(linkedList);
+//   expect(deletedNode.value).toBe("first node");
+//   expect(linkedList.head.value).toBe("second node");
 
-  expect(findNode(linkedList, "first node")).toBe(null);
-  expect(findNode(linkedList, "second node")).toBe(secondNode);
-  expect(length(linkedList)).toBe(2);
+//   expect(findNode(linkedList, "first node")).toBe(null);
+//   expect(findNode(linkedList, "second node")).toBe(secondNode);
+//   expect(length(linkedList)).toBe(2);
 
-  const fourthNode = append(linkedList, "fourth node");
-  expect(length(linkedList)).toBe(3);
-  const deletedNodeTail = removeAfter(linkedList, thirdNode);
-  expect(deletedNodeTail).toBe(fourthNode);
+//   const fourthNode = append(linkedList, "fourth node");
+//   expect(length(linkedList)).toBe(3);
+//   const deletedNodeTail = removeAfter(linkedList, thirdNode);
+//   expect(deletedNodeTail).toBe(fourthNode);
 
-  expect(findNode(linkedList, "fourth node")).toBe(null);
-  expect(findNode(linkedList, "third node")).toBe(thirdNode);
-});
+//   expect(findNode(linkedList, "fourth node")).toBe(null);
+//   expect(findNode(linkedList, "third node")).toBe(thirdNode);
+// });
 
-it("use toMap function to get all nodes", () => {
-  const linkedList = createLinkedList("a");
-  const secondNode = append(linkedList, "b");
-  const thirdNode = append(linkedList, "c");
+// it("use toMap function to get all nodes", () => {
+//   const linkedList = createLinkedList("a");
+//   const secondNode = append(linkedList, "b");
+//   const thirdNode = append(linkedList, "c");
 
-  const expectedResult = [{ value: "a", next: secondNode }, { value: "b", next: thirdNode }, { value: "c", next: null }];
-  expect(toMap(linkedList)).toEqual(expectedResult);
-})
+//   const expectedResult = [{ value: "a", next: secondNode }, { value: "b", next: thirdNode }, { value: "c", next: null }];
+//   expect(toMap(linkedList)).toEqual(expectedResult);
+// })
